a9941ed0efcfceb2c24a2d1e34b2eaf105337b3d
zhaohj <1142466886@qq.com>
2021-03-07 00:14:44 +0800

    20210307

--- src/RotateArray.java
+++ src/RotateArray.java
@@ -22,11 +22,32 @@ public class RotateArray {
         }
     }
 
+    //时间复杂度O(n) 空间复杂度O(1)
+
+    public void rotate2(int[] nums, int k) {
+        int n=nums.length-1;
+        k=k%(n+1);
+        reverse(nums,0,n);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+    }
+
     public static void main(String[] args) {
         RotateArray rotateArray=new RotateArray();
 //        int[] nums={1,2,3,4,5,6,7};
-        int[] nums={-1,-100,3,99};
-//        int[] nums={-1,-100,3,99,22};
-        rotateArray.rotate(nums,5);
+//        int[] nums={-1,-100,3,99};
+        int[] nums={-1,-100,3,99,22};
+//        rotateArray.rotate(nums,5);
+        rotateArray.rotate2(nums,1000);
     }
 }
